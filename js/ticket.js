13bc2530d1a4cc632af85b93f06c0a52666a3a5c
Susan Su <suchangau@hotmail.com>
2021-04-12 15:35:45 +1000

    ticket

---
+++ js/ticket.js
@@ -0,0 +1,43 @@
+//https://rest.bandsintown.com/artists/the%20wiggles?app_id=codingbootcamp
+//https://www.youtube.com/yt/dev/api-resources.html
+
+/*var ticketUrl = 'https://rest.bandsintown.com/artists/the%20wiggles?app_id=codingbootcamp'
+
+fetch(ticketUrl)
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+    .catch(function (error){
+        console.log(error)
+    })
+
+var vedioUrl = 'https://www.youtube.com/yt/dev/api-resources.html?&appid=AIzaSyA-enB949bkk4hCtJmrwpy_dHle7YjH1wQ'
+
+    fetch(vedioUrl)
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+    })
+    .then(function (data){
+        console.log(data);
+    })
+    .catch(function (error){
+        console.log(error)
+    })
+
+    */
+
+document.querySelector('.submit-btn').addEventListener("submit",function(){
+
+    
+
+
+
+
+})
+
+
